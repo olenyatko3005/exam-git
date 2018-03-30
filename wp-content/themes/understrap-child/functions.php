36323be1999809acb0b769dc099a6915070266d0
Olha <olgag3005@gmail.com>
2018-03-30 22:43:43 +0300

    add jquery ui

--- wp-content/themes/understrap-child/functions.php
+++ wp-content/themes/understrap-child/functions.php
@@ -34,7 +34,8 @@ function theme_enqueue_styles() {
 	wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
 	wp_enqueue_script( 'slick-slider', get_stylesheet_directory_uri() . '/libs/slick-carousel/slick.min.js', array(), false);  
-	wp_enqueue_script( 'jquery-ui-style', get_stylesheet_directory_uri() . '/libs/jquery-ui-1.12.1.custom/jquery-ui.min.css', array(), false);  
+	wp_enqueue_style( 'jquery-ui-style', get_stylesheet_directory_uri() . '/libs/jquery-ui-1.12.1.custom/jquery-ui.min.css', array());   
+	wp_enqueue_style( 'jquery-ui-style', get_stylesheet_directory_uri() . '/libs/jquery-ui-1.12.1.custom/jquery-ui.theme.min.css', array());  
 	wp_enqueue_script( 'jquery-ui', get_stylesheet_directory_uri() . '/libs/jquery-ui-1.12.1.custom/jquery-ui.min.js', array(), false);  
    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
